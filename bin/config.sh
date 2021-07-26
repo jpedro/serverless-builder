a62989d58be5519edaa92ef1a85d079267c30efd
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-07-26 17:47:46 +0200

    asdfasdfasdfasdfasdfasdfadsf

--- bin/config.sh
+++ bin/config.sh
@@ -1,8 +1,8 @@
 export IMAGE_TAG="${1:-latest}"
-export IMAGE_REGISTRY="563660588778.dkr.ecr.eu-north-1.amazonaws.com"
-export IMAGE_NAME="di-datalake-builder"
-# export IMAGE_REPO="$IMAGE_REGISTRY/$IMAGE_NAME"
-export IMAGE_REPO="jpedrob/serverless-builder"
+# export IMAGE_REGISTRY="563660588778.dkr.ecr.eu-north-1.amazonaws.com"
+export IMAGE_REGISTRY="jpedrob"
+export IMAGE_NAME="serverless-builder"
+export IMAGE_REPO="$IMAGE_REGISTRY/$IMAGE_NAME"
 export IMAGE_LATEST="$IMAGE_REPO:latest"
 
 green() {
